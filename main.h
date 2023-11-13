89ecaa7c52101b18260ed61a96b54c70bc7941a9
Ayyoub <azimayyoub@gmail.com>
2023-11-13 06:38:14 -0800

    a function that produces output according to a format

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <limits.h>
+
+int _printf(const char *format, ...);
+void print_regular_character(char c, int *count);
+
+#endif
