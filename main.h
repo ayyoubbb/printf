f0975e3385ba302232c5c0da09137f923159f497
Ayyoub <azimayyoub@gmail.com>
2023-11-13 14:47:25 -0800

    a function that produces output according to a format.

--- main.h
+++ main.h
@@ -4,18 +4,6 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-#define CUSTOM_BUFF_SIZE 1024
+int _print(const char *format, ...);
 
-/* Function declarations */
-int my_printf(const char *customFormat, ...);
-int getCustomFlags(const char *customFormat, int *index);
-int getCustomWidth(const char *customFormat, int *index, va_list argsList);
-int getCustomPrecision(const char *customFormat, int *index, va_list argsList);
-int getCustomSize(const char *customFormat, int *index);
-int handleCustomPrint(const char *customFormat, int *index, va_list argsList,
-		      char outputBuffer[], int customFlags, int customWidth,
-		      int customPrecision, int customSize);
-void printOutputBuffer(char outputBuffer[], int *outputBufferIndex);
-
-#endif /* MAIN_H */
-
+#endif
--- print.c
+++ print.c
@@ -1,60 +1,34 @@
 #include "main.h"
+#include <unistd.h>
 
-void printOutputBuffer(char outputBuffer[], int *outputBufferIndex);
+int _printf(const char *format, ...) {
+	va_list args;
+	int count = 0;
 
-/**
- *  * my_printf - Custom printf function
- *   * @customFormat: Custom format.
- *    * Return: Number of characters printed.
- *     */
-int my_printf(const char *customFormat, ...)
-{
-	int i, printedChars = 0, printed = 0;
-	int customFlags, customWidth, customPrecision, customSize, outputBufferIndex = 0;
-	va_list argsList;
-	char outputBuffer[CUSTOM_BUFF_SIZE];
-	if (customFormat == NULL)
-		return (-1);
-	va_start(argsList, customFormat);
-	for (i = 0; customFormat && customFormat[i] != '\0'; i++)
-	{
-		if (customFormat[i] != '%')
-		{
-			outputBuffer[outputBufferIndex++] = customFormat[i];
-			if (outputBufferIndex == CUSTOM_BUFF_SIZE)
-				printOutputBuffer(outputBuffer, &outputBufferIndex);
-			printedChars++;
+	va_start(args, format);
+
+	while (*format) {
+		if (*format == '%' && (*(format + 1) == 'c' || *(format + 1) == 's' || *(format + 1) == '%')) {
+			format++;
+
+			if (*format == 'c') {
+				count += write(1, &va_arg(args, int), 1);
+			} else if (*format == 's') {
+				count += write(1, va_arg(args, char *), 1);
+			} else if (*format == '%') {
+				count += write(1, "%", 1);
+			} else {
+				count += write(1, "%", 1);
+				count += write(1, &(*format), 1);
+			}
+		} else {
+			count += write(1, &(*format), 1);
 		}
-		else
-		{
-			printOutputBuffer(outputBuffer, &outputBufferIndex);
-			customFlags = getCustomFlags(customFormat, &i);
-			customWidth = getCustomWidth(customFormat, &i, argsList);
-			customPrecision = getCustomPrecision(customFormat, &i, argsList);
-			customSize = getCustomSize(customFormat, &i);
-			++i;
-			printed = handleCustomPrint(customFormat, &i, argsList, outputBuffer,
-					customFlags, customWidth, customPrecision,customSize);
-			if (printed == -1)
-				return (-1);
-			printedChars += printed;
-		}
+
+		format++;
 	}
-	printOutputBuffer(outputBuffer, &outputBufferIndex);
 
-	va_end(argsList);
-	return (printedChars);
+	va_end(args);
+
+	return count;
 }
-/**
- *  * printOutputBuffer - Prints the contents of the output buffer if it exists
- *   * @outputBuffer: Array of characters
- *    * @outputBufferIndex: Index at which to add the next character, represents the length.
- *     */
-void printOutputBuffer(char outputBuffer[], int *outputBufferIndex)
-{
-	if (*outputBufferIndex > 0)
-		write(1, &outputBuffer[0], *outputBufferIndex);
-
-	*outputBufferIndex = 0;
-}
-
